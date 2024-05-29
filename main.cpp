8cd3d4aff5dd1008316f438715ac0f2376d14be9
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-05-29 20:09:47 +0200

    Added code to dynamically create array and fill it with odd values

--- main.cpp
+++ main.cpp
@@ -14,5 +14,22 @@ int main() {
     cout << "Enter the number of columns: ";
     cin >> cols;
 
+    int** arr = new int* [rows];
+for (int i = 0; i < rows; ++i) {
+    arr[i] = new int[cols];
+}
+
+int value = 1;
+for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+        while (!isOdd(value)) {
+            value++;
+        }
+        arr[i][j] = value;
+        value += 2;
+    }
+}
+
+
     return 0;
 }
