c176234e69962dbe2d1c9d33628c0524914d6b35
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-05-29 20:08:16 +0200

    Added main function to get user input

--- main.cpp
+++ main.cpp
@@ -5,3 +5,14 @@ using namespace std;
 static bool isOdd(int num) {
     return num % 2 != 0;
 }
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+
+    return 0;
+}
